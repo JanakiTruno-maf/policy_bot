3cd91fe0a61a55a0001af10195a0b6f476fd0605
JanakiTruno-maf <Janaki.Truno@mafgroup.co.uk>
2025-09-19 23:02:30 +0530

    Remove invalid invoker_iam_disabled and fix lock handling

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,10 +21,9 @@ provider "google" {
 
 # Simple Cloud Run service deployment
 resource "google_cloud_run_v2_service" "default" {
-  name                = var.service_name
-  location            = var.region
-  ingress             = "INGRESS_TRAFFIC_ALL"
-  invoker_iam_disabled = false
+  name     = var.service_name
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
     timeout = "300s"
